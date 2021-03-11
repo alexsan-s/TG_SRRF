8d2ff2c7cbee230b0b3488c8eabeeee624f809b6
Alexsander da Silva <alexsans@outlook.com.br>
2021-03-10 21:17:06 -0300

    NA

--- lib/view/training.py
+++ lib/view/training.py
@@ -23,9 +23,9 @@ def getImageId():
 # ! Will create tree archive yml
 #
 def recognitionTraining():
-    eigenface = cv2.face.EigenFaceRecognizer_create()
-    fisherface = cv2.face.FisherFaceRecognizer_create()
-    lbph = cv2.face.LBPHFaceRecognizer_create()
+    eigenface   = cv2.face.EigenFaceRecognizer_create()
+    fisherface  = cv2.face.FisherFaceRecognizer_create()
+    lbph        = cv2.face.LBPHFaceRecognizer_create()
 
     # layout
     layout = [
