fcccf5f55e3e5069ddd1a568e9d8325d3b2ac84e
Alexsander da Silva <alexsans@outlook.com.br>
2021-04-02 14:22:32 -0300

    Create Product

--- lib/controller/toolbar.py
+++ lib/controller/toolbar.py
@@ -2,7 +2,7 @@ def toolbarMenu(toolbar):
     # ! TOOLBAR MENU
     if toolbar == 'home':
         toolbar_menu = [
-            ['File', ['Client', 'Operator', 'Images', 'Training','Import Data', 'Exit']],
+            ['File', ['Client', 'Operator', 'Product', 'Images', 'Training','Import Data', 'Logout', 'Exit',]],
             ['Recognition', ['Eigenfaces', 'Fisherface', 'LBPH']]
         ]
     return toolbar_menu
