b21ee7b8f5ba76fd3635755e55211e9b01be3a0a
Alexsander da Silva <alexsans@outlook.com.br>
2021-05-25 22:58:17 -0300

    using pandas

--- lib/controller/importXLSX.py
+++ lib/controller/importXLSX.py
@@ -1,14 +1,16 @@
 import openpyxl
 import hashlib
+import pandas as pd
 from datetime import datetime
 from controller.database.conf import *
 from controller import function
 
 def importXLSX(table, file_path):
     try:
-        wb = openpyxl.load_workbook(filename=file_path)
+        sheet = pd.read_excel(file_path)
+        # wb = openpyxl.load_workbook(filename=file_path, read_only=True)
 
-        sheet = wb.active
+        # sheet = wb.active
 
         if table == 'Operator':
             return importOperator(sheet)
@@ -22,39 +24,38 @@ def importXLSX(table, file_path):
 
 def importOperator(sheet):
     try:
-        row = 2
         msg = ''
 
         conf = configurationElephant()
         cur = conf.cursor()
 
         # Pass for all columns
-        for y in range(row, 1000):
+        for index, row in sheet.iterrows():
             error = 0
 
             # 1
-            temp = sheet.cell(row=y, column= 1).value
+            temp = row[0]
             if temp == None:
                 break
             name = function.capitalizeWord(temp)
             
             # 2
-            temp    = sheet.cell(row=y, column = 2).value
+            temp    = row[1]
             if temp == None:
                 if temp:
                     telefone = temp
             else:
                 temp = function.validadeTelefone(str(temp))
                 if temp:
-                    telefone = sheet.cell(row=y, column = 2).value
+                    telefone = row[1]
                 else:
-                    msg += 'Erro row {}: The phone[{}] field was not filled in correctly.\n'.format(str(y), temp)
+                    msg += 'Erro row {}: The phone[{}] field was not filled in correctly.\n'.format(str(index), temp)
                     error += 1
 
             # 3
-            temp         = sheet.cell(row=y, column = 3).value
+            temp         = row[2]
             if temp == None:
-                msg += 'Error row {}: The CPF[{}] field was not filled in correctly.\n'.format(str(y), temp)
+                msg += 'Error row {}: The CPF[{}] field was not filled in correctly.\n'.format(str(index), temp)
                 error += 1
             else:
                 cpf     = function.validadeCPF(str(temp))
@@ -64,17 +65,17 @@ def importOperator(sheet):
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Erro linha {}: O CPF[{}] already exists in the database and cannot be inserted.\n'.format(str(y), cpf)
+                        msg += 'Erro row {}: The CPF[{}] already exists in the database and cannot be inserted.\n'.format(str(index), cpf)
                         error += 1
                 else:
-                    msg += 'Erro row {}: The cpf[{}] field was not filled in correctly.\n'.format(str(y), cpf)
+                    msg += 'Erro row {}: The cpf[{}] field was not filled in correctly.\n'.format(str(index), cpf)
                     error += 1
 
             # 4
             if error == 0:
-                temp       = sheet.cell(row=y, column = 4).value
+                temp       = row[3]
                 if temp == None:
-                    msg += 'Erro linha {}: O campo email[{}] não está preenchido.\n'.format(str(y), temp)
+                    msg += 'Erro linha {}: O campo email[{}] não está preenchido.\n'.format(str(index), temp)
                     error += 1
                 else:
                     if function.validadeEmail(str(temp)):
@@ -83,17 +84,17 @@ def importOperator(sheet):
                         cur.execute(sql)
                         rows = cur.fetchall()
                         if rows:
-                            msg += 'Erro linha {}: O EMAIL[{}] already exists in the database and cannot be inserted.\n'.format(str(y), email)
+                            msg += 'Erro linha {}: O EMAIL[{}] already exists in the database and cannot be inserted.\n'.format(str(index), email)
                             error += 1
                     else:
-                        msg += 'Erro row {}: The email[{}] field was not filled in correctly.\n'.format(str(y), email)
+                        msg += 'Erro row {}: The email[{}] field was not filled in correctly.\n'.format(str(index), email)
                         error += 1
 
             # 5
             if error == 0:
-                temp       = sheet.cell(row=y, column = 5).value
+                temp       = row[4]
                 if temp == None:
-                    msg += 'Error row {}: The login[{}] field was not filled in correctly.\n'.format(str(y), temp)
+                    msg += 'Error row {}: The login[{}] field was not filled in correctly.\n'.format(str(index), temp)
                     error += 1
                 else:
                     login = function.capitalizeWord(str(temp))
@@ -101,31 +102,31 @@ def importOperator(sheet):
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The login[{}] already exists in the database and cannot be inserted.\n'.format(str(y), login)
+                        msg += 'Error row {}: The login[{}] already exists in the database and cannot be inserted.\n'.format(str(index), login)
                         error += 1
 
             # 6
             if error == 0:
-                temp    = sheet.cell(row=y, column = 6).value
+                temp    = row[5]
                 if temp == None:
                     temp = 'Ch@nge123!'
                 if function.validadePassword(temp):
                     password = temp
                     pass_hash = hashlib.sha1(password.encode('utf-8')).hexdigest()
                 else:
-                    msg += 'Erro row {}: The password[{}] field was not filled in correctly.\n'.format(str(y), email)
+                    msg += 'Erro row {}: The password[{}] field was not filled in correctly.\n'.format(str(index), email)
                     error += 1
 
 
             # 7
             if error == 0:
-                temp    = sheet.cell(row=y, column = 7).value
+                temp    = row[6]
                 if temp == None:
                     inactive = 0
                 if function.validadeNumber(str(temp)):
                     inactive = temp
                 else:
-                    incative = 0
+                    inactive = 0
 
 
             if error == 0:
@@ -135,10 +136,10 @@ def importOperator(sheet):
                 try:
                     cur.execute(sql)
                     conf.commit()
-                    msg += 'Ok linha {}: O Operador[{}] foi cadastrado com sucesso.\n'.format(str(y), name)
+                    msg += 'Ok linha {}: O Operador[{}] foi cadastrado com sucesso.\n'.format(str(index), name)
                 except:
-                    msg += 'Erro linha {}: O Operador[{}] não foi possível ser cadastrado.\n'.format(str(y), name)
-        fileName = "ImportOperator_{}.txt".format(str(datetime.now()))
+                    msg += 'Erro linha {}: O Operador[{}] não foi possível ser cadastrado.\n'.format(str(index), name)
+        fileName = "ImportOperator_{}.txt".format(str(datetime.now().date()))
         file = open(fileName, "w") 
         file.write(str(msg))
         file.close
@@ -150,24 +151,23 @@ def importOperator(sheet):
     
 def importProduct(sheet):
     try:
-        row = 2
         msg = ''
 
         conf = configurationElephant()
         cur = conf.cursor()
 
-        # Pass for all columns
-        for y in range(row, 1000):
+        # Pass for all columns        
+        for index, row in sheet.iterrows():
             error = 0
 
             # 1
-            tempProduct = sheet.cell(row=y, column= 1).value
+            tempProduct = row[0]
             if tempProduct == None:
                 break
             product = function.capitalizeWord(tempProduct)
             
             # 2
-            tempDescription    = sheet.cell(row=y, column = 2).value
+            tempDescription    = row[1]
             if tempDescription == None:
                 description = ''
             else:
@@ -176,7 +176,7 @@ def importProduct(sheet):
             
 
             # 3
-            inactive    = sheet.cell(row=y, column = 3).value
+            inactive    = row[2]
             if inactive == None:
                 inactive = 0
 
@@ -187,9 +187,9 @@ def importProduct(sheet):
                 try:
                     cur.execute(sql)
                     conf.commit()
-                    msg += 'Ok row {}: The product [{}] was registered sucefull.\n'.format(str(y), product)
+                    msg += 'Ok row {}: The product [{}] was registered sucefull.\n'.format(str(index), product)
                 except:
-                    msg += 'Error row {}: The product[{}] was not registered with successful.\n'.format(str(y), product)
+                    msg += 'Error row {}: The product[{}] was not registered with successful.\n'.format(str(index), product)
         fileName = "ImportProduct{}.txt".format(str(datetime.now()))
         file = open(fileName, "w") 
         file.write(str(msg))
@@ -202,114 +202,113 @@ def importProduct(sheet):
 
 def importClient(sheet):
     try:
-        row = 2
         msg = ''
 
         conf = configurationElephant()
         cur = conf.cursor()
 
         # Pass for all columns
-        for y in range(row, 1000):
+        for index, row in sheet.iterrows():
             error = 0
 
             # 1
-            name        = sheet.cell(row=y, column= 1).value
+            name        = row[0]
             if name == None:
                 break
 
             # 2
             if error == 0:
-                cpf       = sheet.cell(row=y, column = 2).value
+                cpf       = row[1]
                 if cpf == None:
-                    msg += 'Error row {}: The cell cpf[{}] wasnt preench.\n'.format(str(y), cpf)
+                    msg += 'Error row {}: The cell cpf[{}] wasnt preench.\n'.format(str(index), cpf)
                     error += 1
                 else:
                     sql = "SELECT CPF FROM CLIENT WHERE CPF = '{}'".format(cpf)
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The cpf[{}] was exists in the database and cannot register.\n'.format(str(y), cpf)
+                        msg += 'Error row {}: The cpf[{}] was exists in the database and cannot register.\n'.format(str(index), cpf)
                         error += 1
 
             # 3
             if error == 0:
-                rg       = sheet.cell(row=y, column = 3).value
+                rg       = row[2]
                 if rg == None:
-                    msg += 'Error row {}: The cell rg[{}] wasnt preench.\n'.format(str(y), rg)
+                    msg += 'Error row {}: The cell rg[{}] wasnt preench.\n'.format(str(index), rg)
                     error += 1
                 else:
                     sql = "SELECT RG FROM CLIENT WHERE RG = '{}'".format(rg)
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The rg[{}] was exists in the database and cannot register.\n'.format(str(y), rg)
+                        msg += 'Error row {}: The rg[{}] was exists in the database and cannot register.\n'.format(str(index), rg)
                         error += 1
                 
             # 4
-            birth       = sheet.cell(row=y, column= 4).value
+            birth       = row[3]
             
             # 5
-            sex         = sheet.cell(row=y, column= 5).value
+            sex         = row[4]
             
 
             # 6
             if error == 0:
-                email   = sheet.cell(row=y, column = 6).value
+                email   = row[5]
                 if email == None:
-                    msg += 'Error row {}: The cell email[{}] wasnt preench.\n'.format(str(y), email)
+                    msg += 'Error row {}: The cell email[{}] wasnt preench.\n'.format(str(index), email)
                     error += 1
                 else:
                     sql = "SELECT EMAIL FROM CLIENT WHERE EMAIL = '{}'".format(email)
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The email[{}] was exists in the database and cannot register.\n'.format(str(y), email)
+                        msg += 'Error row {}: The email[{}] was exists in the database and cannot register.\n'.format(str(index), email)
                         error += 1
                     
             # 7
-            cep         = sheet.cell(row=y, column= 7).value
+            cep         = row[6]
             
             # 8
-            address     = sheet.cell(row=y, column= 8).value
+            address     = row[7]
 
             # 9
-            number      = sheet.cell(row=y, column= 9).value
+            number      = row[8]
 
             # 10
-            district    = sheet.cell(row=y, column= 10).value
+            district    = row[9]
 
             # 11
-            city        = sheet.cell(row=y, column= 11).value
+            city        = row[10]
 
             # 12
-            state       = sheet.cell(row=y, column= 12).value
+            state       = row[11]
 
             # 13
             if error == 0:
-                telefone    = sheet.cell(row=y, column = 13).value
+                telefone    = row[12]
                 if email == None:
-                    msg += 'Error row {}: The cell telefone[{}] wasnt preench.\n'.format(str(y), telefone)
+                    msg += 'Error row {}: The cell telefone[{}] wasnt preench.\n'.format(str(index), telefone)
                     error += 1
                 else:
                     sql = "SELECT TELEFONE FROM CLIENT WHERE TELEFONE = '{}'".format(telefone)
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The telefone[{}] was exists in the database and cannot register.\n'.format(str(y), telefone)
+                        msg += 'Error row {}: The telefone[{}] was exists in the database and cannot register.\n'.format(str(index), telefone)
                         error += 1
 
             # 14
             if error == 0:
-                cell    = sheet.cell(row=y, column = 14).value
+                cell    = row[13]
                 if email == None:
-                    msg += 'Error row {}: The cell cell[{}] wasnt preench.\n'.format(str(y), cell)
+                    msg += 'Error row {}: The cell cell[{}] wasnt preench.\n'.format(str(index), cell)
                     error += 1
                 else:
                     sql = "SELECT CELL FROM CLIENT WHERE CELL = '{}'".format(cell)
                     cur.execute(sql)
                     rows = cur.fetchall()
                     if rows:
-                        msg += 'Error row {}: The cell[{}] was exists in the database and cannot register.\n'.format(str(y), cell)
+                        msg += 'Error row {}: The cell[{}] was exists in the database and cannot register.\n'.format(str(index), cell)
                         error += 1
 
             if error == 0:
@@ -319,10 +318,10 @@ def importClient(sheet):
                 try:
                     cur.execute(sql)
                     conf.commit()
-                    msg += 'Ok row {}: The client[{}] has been successfully registered.\n'.format(str(y), name)
+                    msg += 'Ok row {}: The client[{}] has been successfully registered.\n'.format(str(index), name)
                 except:
-                    msg += 'Error row {}: The client[{}] could not be registered.\n'.format(str(y), name)
-        fileName = "ImportClient_{}.txt".format(str(datetime.now()))
+                    msg += 'Error row {}: The client[{}] could not be registered.\n'.format(str(index), name)
+        fileName = "ImportClient_{}.txt".format(str(datetime.now().date()))
         file = open(fileName, "w") 
         file.write(str(msg))
         file.close
