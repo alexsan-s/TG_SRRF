59cd65e49418c4a6ac869d3bcda5bd82d4e5aa79
Alexsander da Silva <alexsans@outlook.com.br>
2021-05-27 07:55:16 -0300

    creating setting

--- lib/controller/toolbar.py
+++ lib/controller/toolbar.py
@@ -2,7 +2,7 @@ def toolbarMenu(toolbar):
     # ! TOOLBAR MENU
     if toolbar == 'home':
         toolbar_menu = [
-            ['File', ['Training','Import Data', 'Logout', 'Exit',]],
+            ['File', ['Training','Import Data', 'Setting', 'Logout', 'Exit',]],
             ['View', ['Client', 'Operator', 'Product', 'Images', 'Recognition', ['Eigenfaces', 'Fisherface', 'LBPH']]],
             ['Insert', ['Buy']]
         ]
---
+++ lib/view/setting.py
@@ -0,0 +1,6 @@
+import PySimpleGUI as sg
+
+def screen():
+    layout = [
+        
+    ]
