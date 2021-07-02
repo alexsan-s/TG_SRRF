1f3971da8f538284d3c95a6e8bdedcdd5b887e19
Alexsander da Silva <alexsans@outlook.com.br>
2021-07-01 21:05:54 -0300

    testando a classe datetime para utilizar futuramente

--- test/teste.py
+++ test/teste.py
@@ -1,3 +1,11 @@
-from datetime import date
+from datetime import datetime
 
-print(date.today())
+date_time_str = '18/09/19 01:55:19.410430'
+
+date_time_obj = datetime.strptime(date_time_str, '%d/%m/%y %H:%M:%S.%f')
+
+
+print ("The type of the date is now",  type(date_time_obj))
+print ("The date is", date_time_obj)
+
+print(datetime.now())
