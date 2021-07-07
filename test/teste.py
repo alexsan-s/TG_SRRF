39f53976d65ff3daa493a0cbf08c3ab0827eac49
Alexsander da Silva <alexsans@outlook.com.br>
2021-07-06 21:51:06 -0300

    testes de data

--- test/teste.py
+++ test/teste.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-date_time_str = '2021-07-01 21:06:16.383707'
+date_time_str = '1992-06-22 00:00:00'
 
-date_time_obj = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+date_time_obj = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
 
 
 print ("The type of the date is now",  type(date_time_obj))
 print ("The date is", date_time_obj)
 
-print(datetime.now())
+print(datetime.now().date())
